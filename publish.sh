b90101edc0da35db93953ef8afe1c676d798b602
Sunny <darkowlzz@protonmail.com>
2020-01-29 17:27:59 +0530

    source key at deployment

--- publish.sh
+++ publish.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-set -Eeuxo pipefail
+set -e
 
-docker login -u "$REGISTRY_USER" -p "$REGISTRY_KEY" "$CONTAINER_REGISTRY"
+source key
+echo "$REGISTRY_KEY" | docker login -u "$REGISTRY_USER" --password-stdin "$CONTAINER_REGISTRY"
 NEW_IMAGE="$CONTAINER_REGISTRY/$PID/kube-scheduler:$CONTAINER_TAG"
 docker tag storageos/kube-scheduler:latest "$NEW_IMAGE"
 docker push "$NEW_IMAGE"
