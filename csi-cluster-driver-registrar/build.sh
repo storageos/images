219a2a6d4a4b9dcc541132ec2f543fa0ba28e588
Simon Croome <simon.croome@storageos.com>
2020-02-05 18:37:37 +0000

    Add csi-cluster-driver-registrar

---
+++ csi-cluster-driver-registrar/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Use separate build script to avoid building windows binary. Builds fail on CIs
+# with limited resources. The default make targets have linux and windows build
+# commands coupled.
+
+set -Eeuxo pipefail
+
+REV=$(git describe --long --tags --match='v*' --dirty 2>/dev/null || git rev-list -n1 HEAD)
+mkdir -p bin
+LDFLAGS="-X main.version=${REV} -extldflags \"-static\""
+CGO_ENABLED=0 GOOS=linux go build -v -a -ldflags "${LDFLAGS}" -o ./bin/cluster-driver-registrar ./cmd/cluster-driver-registrar
